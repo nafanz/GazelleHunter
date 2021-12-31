8f613c3e36188b2ce478ce27d9714aa0ae167f0e
nafanz <nafanz@mail.ru>
2021-12-31 12:03:32 +0500

    ChromeDriver отключил логирование

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 def web(mode):
     if mode == 'tor':
         options = Options()
@@ -10,18 +11,20 @@ def web(mode):
         options.add_argument('--proxy-server=socks5://127.0.0.1:9150')
         # options.add_argument("headless")
         driver = webdriver.Chrome(
-            ChromeDriverManager().install(),
+            ChromeDriverManager(log_level=0).install(),
             options=options
         )
     else:
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver = webdriver.Chrome(ChromeDriverManager(log_level=0).install())
 
     return driver
+
 
 def authorization(driver, object, username, password, login):
     driver.find_element(By.XPATH, username).send_keys(f"{object['login']}")
     driver.find_element(By.XPATH, password).send_keys(f"{object['password']}")
     driver.find_element(By.XPATH, login).click()
+
 
 def send(driver, text, title, body, button):
     driver.find_element(By.XPATH, title).send_keys("Трекеры")
