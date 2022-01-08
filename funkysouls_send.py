b058796c1e32190911462da85949655de0774f9a
nafanz <nafanz@mail.ru>
2022-01-08 19:38:43 +0500

    FS отправка сообщений

--- funkysouls_send.py
+++ funkysouls_send.py
@@ -1,49 +1,32 @@
 import time
-from web import web, authorization
+from web import web
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementNotInteractableException
 from db import select_one_user_to_send, successfully_sent, count_is_null
 from credentials import funkysouls, question, trackers
 
 
-# Переходим на страницу авторизации
+# Заполняем форму авторизации
 driver = web('tor')
 driver.get(f"{funkysouls['url']}/login")
-
-# Для удобства сохраняем XPath формы авторизации
-username = '//*[@id="username"]'
-password = '//*[@id="password"]'
-login = '//*[@id="login-form"]/div/button'
-
-# Заполняем форму авторизации
-authorization(driver, funkysouls, username, password, login)
-
-# Для удобства сохраняем XPath формы отправки сообщения
-title = '//*[@id="content"]/div[3]/div/form/div[2]/fieldset/input'
-body = '//*[@id="answer_form"]'
-checkbox = '//*[@id="add_tracking"]'
-button = '//*[@id="answer"]/div[2]/div[3]/input[1]'
-pop_up = '/html/body/div[2]/div[1]/span'
-
-# Подготавливаем полный текст сообщения
-text = question['ru'] + '\n'.join(trackers)
-
-# Указываем таблицу
-table = 'funkysouls'
+driver.find_element(By.ID, 'username').send_keys(f"{funkysouls['login']}")
+driver.find_element(By.ID, 'password').send_keys(f"{funkysouls['password']}")
+driver.find_element(By.CSS_SELECTOR, '.controls > button:nth-child(1)').click()
 
 while True:
-    user_id = select_one_user_to_send(table)
+    user_id = select_one_user_to_send('funkysouls')
     driver.get(f"{funkysouls['url']}/u/{user_id}/pm")
     # Заполнение формы отправки сообщения
     try:
-        driver.find_element(By.XPATH, title).send_keys('Трекеры')
-        driver.find_element(By.XPATH, body).send_keys(text)
-        driver.find_element(By.XPATH, checkbox).click()
-        driver.find_element(By.XPATH, button).click()
-        successfully_sent(table, user_id)
-        count_is_null(table)
+        driver.find_element(By.NAME, 'msg_title').send_keys('Частные торрент-трекеры')
+        driver.find_element(By.ID, 'answer_form').send_keys(question['ru'] + '\n'.join(trackers))
+        driver.find_element(By.ID, 'add_tracking').click()
+        driver.find_element(By.NAME, 'submit').click()
+        successfully_sent('funkysouls', user_id)
+        count_is_null('funkysouls')
         # Ожидание
         time.sleep(10)
     # Закрытие попапа о новом сообщении, который прерывает отправку
     except ElementNotInteractableException:
-        driver.find_element(By.XPATH, pop_up).click()
+        driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/span').click()
+
