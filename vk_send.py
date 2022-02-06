2b0196b4fd2d787342c5ef55e2aece37d763c433
nafanz <nafanz@mail.ru>
2022-02-06 13:17:10 +0500

    Нужны обработки ошибок

--- vk_send.py
+++ vk_send.py
@@ -1,6 +1,7 @@
 from db import select_one_user_to_send, successfully_sent, count_is_null
 from credentials import vk, question, trackers
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from web import web
 import time
 
@@ -14,16 +15,17 @@ time.sleep(10)
 
 # Подготавливаем полный текст сообщения
 text = question['ru'] + '\n'.join(trackers)
-print(text)
 
 while True:
     user_id = select_one_user_to_send('vk')
-    driver.get(f"{vk['url']}/im?sel={user_id}")
-    time.sleep(10)
-    driver.find_element(By.CLASS_NAME, 'im_editable.im-chat-input--text._im_text').send_keys(text)
-    time.sleep(10)
-    driver.find_element(By.CLASS_NAME, 'im-send-btn.im-chat-input--send._im_send.im-send-btn_send').click()
-    time.sleep(10)
+    driver.get(f"{vk['url']}/id{user_id}")
+    time.sleep(5)
+    driver.find_element(By.CLASS_NAME, 'FlatButton__content').click()
+    time.sleep(5)
+    driver.find_element(By.ID, 'mail_box_editable').send_keys(text)
+    time.sleep(5)
+    driver.find_element(By.CLASS_NAME, 'FlatButton__content').click()
+    time.sleep(5)
     successfully_sent('vk', user_id)
 
 # count_is_null('vk')
