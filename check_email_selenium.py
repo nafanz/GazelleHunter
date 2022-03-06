37258d3950bc98e8e109087a51377bfc78c78a69
nafanz <nafanz@mail.ru>
2022-03-06 15:59:58 +0500

    cock.li

--- check_email_selenium.py
+++ check_email_selenium.py
@@ -4,7 +4,7 @@ import misc
 import credentials
 
 
-for mail in credentials.mails.items():
+for mail in credentials.mail_ru.items():
     driver = misc.web_surfing()
     driver.get('https://account.mail.ru')
     time.sleep(5)
@@ -13,5 +13,15 @@ for mail in credentials.mails.items():
     time.sleep(5)
     driver.find_element(By.NAME, 'password').send_keys(mail[1])
     driver.find_element(By.XPATH, '//span[text()="Войти"]').click()
+    time.sleep(60)
+    driver.quit()
+
+for mail in credentials.cock_li.items():
+    driver = misc.web_surfing(tor=True)
+    driver.get('https://mail.cock.li')
+    time.sleep(5)
+    driver.find_element(By.NAME, '_user').send_keys(mail[0])
+    driver.find_element(By.NAME, '_pass').send_keys(mail[1])
+    driver.find_element(By.ID, 'rcmloginsubmit').click()
     time.sleep(60)
     driver.quit()
