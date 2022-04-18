4ba6a55a31bae4e7f5e4cd11c818d622e4707ed1
nafanz <nafanz@mail.ru>
2022-04-18 13:59:40 +0500

    API rate limit exceeded

--- github.py
+++ github.py
@@ -1,23 +1,42 @@
-import math
+import re
 import requests
 
 
-repos = 'HDVinnie/Private-Trackers-Spreadsheet'
-per_page = 100
-page = math.ceil(311/per_page)
-
 email = set()
+email_regex = '[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'
+page = None
+repos_api = 'https://api.github.com/repos'
+repos_list = [
+    'HDVinnie/Private-Trackers-Spreadsheet'
+]
 
-for x in range(1, page+1):
-    code = requests.get(f'https://api.github.com/repos/{repos}/commits?page={x}&per_page={per_page}').json()
-    try:
-        for y in code:
-            if not y['commit']['author']['email'].endswith('github.com'):
-                email.add(y['commit']['author']['email'])
-            if not y['commit']['committer']['email'].endswith('github.com'):
-                email.add(y['commit']['committer']['email'])
-    except TypeError:
-        pass
+for repos in repos_list:
+    page_list = requests.get(f'{repos_api}/{repos}/commits?page=1&')
+    if page_list.status_code != 200:
+        print(page_list.status_code, page_list.json()['message'])
+        break
+    else:
+        page = int((re.findall('\d+\Z', page_list.links['last']['url']))[0])
 
-for x in email:
-    print(x)
+    for x in range(1, page+1):
+        code = requests.get(f'{repos_api}/{repos}/commits?page={x}').json()
+        try:
+            for y in code:
+                email_author = y['commit']['author']['email']
+                if not email_author.endswith('github.com'):
+                    print(email_author)
+                    email_re = re.findall(email_regex, email_author)
+                    print(email_re)
+                    # email.add(re.findall(email_regex, email_author))
+
+                email_committer = y['commit']['committer']['email']
+                if not email_committer.endswith('github.com'):
+                    print(email_committer)
+                    email_re = re.findall(email_regex, email_committer)
+                    print(email_re)
+                    # email.add(re.findall(email_regex, email_committer))
+        except TypeError:
+            pass
+
+# for x in email:
+#     print(x)
